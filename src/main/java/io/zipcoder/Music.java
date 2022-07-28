17a8438d1fcb7c55a1948052ede8c3e0e2e44333
kylehwelch <welch@udel.edu>
2022-07-27 22:18:15 -0400

    tests passed by counter forward and backward

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,18 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int countUp = 0;
+        int countBack = 0;
+        for (int i = startIndex; i < playList.length; i++) {
+            if (playList[i] == selection) break;
+            else countUp++;
+        }
+        for (int i = startIndex; i < playList.length; i--) {
+            if (playList[i] == selection) break;
+            else countBack++;
+            if (i == 0) i = playList.length;
+        }
+        if (countBack < countUp && countBack >= 0) return countBack;
+        else return countUp;
     }
 }
